b0af88ddc04e2491f1b492d6eda3a1c747bdca44
AmosBarreto <143368467+AmosBarreto@users.noreply.github.com>
2023-09-29 23:42:23 -0300

    Update imccalculo.php
    Alunos: 
    Amós Barreto Brandão
    Igor Fernandes
    Samuel Andrade

--- imccalculo.php
+++ imccalculo.php
@@ -1,46 +1,96 @@
+DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <?php
+        function parseNumber($number) {
+            return str_replace(',', '.', $number);
+        }
+        if (isset($_POST["peso"]) && isset($_POST["altura"])) {
+            $peso = parseNumber($_POST["peso"]);
+            $altura = parseNumber($_POST["altura"]);
+
+           
+            $imc = $peso / ($altura * $altura);
+
+           
+            echo "Seu IMC é: " . number_format($imc, 2) . "<br><br>";
+
+            if ($imc < 17) {
+                echo "Situação: Muito abaixo do peso";
+            } elseif ($imc >= 17 && $imc < 18.5) {
+                echo "Situação: Abaixo do peso";
+            } elseif ($imc >= 18.5 && $imc < 25) {
+                echo "Situação: Peso normal";
+            } elseif ($imc >= 25 && $imc < 30) {
+                echo "Situação: Acima do peso";
+            } elseif ($imc >= 30 && $imc < 35) {
+                echo "Situação: Obesidade I";
+            } elseif ($imc >= 35 && $imc < 40) {
+                echo "Situação: Obesidade II (severa)";
+            } else {
+                echo "Situação: Obesidade III (mórbida)";
+            }
+    }
+    ?>
+    <h2>Calculadora de IMC</h2>
+    <form action="upload.php" method="post">
+        <label for="peso">Peso (em quilos):</label>
+        <input type="text" name="peso" id="peso" required><br><br>
+       
+        <label for="altura">Altura (em metros):</label>
+        <input type="text" name="altura" id="altura" required><br><br>
+       
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+</html>
+
+2)  
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 <head>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cálculo do IMC</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">>
+    <title>Calculadora de Média</title>
 </head>
 <body>
-    <?php
-        $peso=$_POST['peso'];
-        $altura=$_POST['altura'];
-        $imc=$peso/pow($altura,2);
-        str_replace($peso, $altura);
-        echo "<h1 class='text-center'>Resultado</h1>";
-        if ($imc<17){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Muito abaixo do Peso</h3>";
+<?php
+    function nmrflutuante($number) {
+        return str_replace(',', '.', $number);
+    }
+    if (isset($_POST["nome"]) && isset($_POST["nota1"]) && isset($_POST["nota2"])) {
+        $nome = $_POST["nome"];
+        $nota1 = nmrflutuante($_POST["nota1"]);
+        $nota2 = nmrflutuante($_POST["nota2"]);
+
+       
+        $media = ($nota1 + $nota2) / 2;
+
+       
+        echo "Aluno: " . $nome . "<br><br>";
+        echo "Média: " . number_format($media, 2) . "<br><br>";
+
+        if ($media < 4) {
+            echo "Situação: Reprovado";
+        } elseif ($media >= 4 && $media < 6) {
+            echo "Situação: Exame final";
+        } else {
+            echo "Situação: Aprovado";
         }
-        else if($imc>=17 and $imc<=18.49){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Abaixo do Peso</h3>";
-        }
-        else if($imc>=18.5 and $imc<=24.99){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Peso Normal</h3>";
-        }
-        else if($imc>=25 and $imc<=29.99){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Acima do Peso</h3>";
-        }
-        else if($imc>=30 and $imc<=34.99){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Obesidade I</h3>";
-        }
-        else if($imc>=35 and $imc<=39.99){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Obesidade II (severa)</h3>";
-        }
-        else($imc>=40){
-            echo "<input type='text' class='form-control col-6 mx-auto' value=" .$imc. ">";
-            echo "<h3 class='text-center'>Obesidade III (Mórbida)</h3>";
-        }
-    ?>    
+    }
+    ?>
+    <h2>Calculadora de Média</h2>
+    <form action="upload.php" method="post">
+        <label for="nome">Nome do Aluno:</label>
+        <input type="text" name="nome" id="nome" required><br><br>
+       
+        <label for="nota1">Nota 1:</label>
+        <input type="text" name="nota1" id="nota1" required><br><br>
+       
+        <label for="nota2">Nota 2:</label>
+        <input type="text" name="nota2" id="nota2" required><br><br>
+       
+        <input type="submit" value="Calcular Média">
+    </form>
 </body>
 </html>
